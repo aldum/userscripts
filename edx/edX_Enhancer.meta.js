e8d4f085eab2f1acbc180c1f812c433739963490
aldum <aldum@sch.bme.hu>
2018-10-19 01:30:14 +0200

    init edXE

---
+++ edx/edX_Enhancer.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          edX Enhancer
+// @namespace     http://github.com/aldum/userscripts
+// @version       0.1
+// @description   edX enhancing scriptery
+// @author        aldum
+// @match         https://courses.edx.org/courses/*/progress
+// @updateURL     https://github.com/aldum/userscripts/raw/master/edx/edX_Enhancer.meta.js
+// @downloadURL   https://github.com/aldum/userscripts/raw/master/edx/edX_Enhancer.user.js
+// @grant        none
+// ==/UserScript==
+
---
+++ edx/edX_Enhancer.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name          edX Enhancer
+// @namespace     http://github.com/aldum/userscripts
+// @version       0.1
+// @description   edX enhancing scriptery
+// @author        aldum
+// @match         https://courses.edx.org/courses/*/progress
+// @updateURL     https://github.com/aldum/userscripts/raw/master/edx/edX_Enhancer.meta.js
+// @downloadURL   https://github.com/aldum/userscripts/raw/master/edx/edX_Enhancer.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('#course-info-progress > section  dl dd').each((v, e) => {
+        const el = $(e);
+        const n = eval(el.html());
+        (n == 1 | Number.isNaN(n)) ? el.css('color','black') : el.css('color','#A70131', 'font-weight', 'bold');
+    });
+})();
+
